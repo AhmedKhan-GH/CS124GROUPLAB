d6d0d41b146154769a182dd99942bba94dd837fc
Ahmed <emailahmedebadkhan@gmail.com>
2023-04-25 17:49:58 -0700

    info lambda, waypoint class rudiments

--- src/Runtime.cpp
+++ src/Runtime.cpp
@@ -1,7 +1,6 @@
 #include "Runtime.hpp"
 
-Runtime::Runtime() : run_state(false)
-{}
+Runtime::Runtime() : run_state(true){}
 
 void Runtime::addCommand(const std::string& key, Command code)
 {
@@ -18,6 +17,7 @@ void Runtime::addLambdas()
 		std::cout <<
 		std::endl << "help: lists available commands" <<
 		std::endl << "exit: terminates the program" <<
+		std::endl << "info: learn more about the program" <<
 		std::endl;
 	};
 	addCommand("help", helpLambda);
@@ -30,12 +30,29 @@ void Runtime::addLambdas()
 		std::endl;
 	};
 	addCommand("exit", exitLambda);
+
+	auto infoLambda = [](const std::vector<std::string>& params)
+	{
+		std::cout << 
+		std::endl << "Thank you for choosing our program." <<
+		std::endl << "Eagle Eye is a command line based" <<
+		std::endl << "waypoint mapper and  editor that lets" <<
+		std::endl << "you keep track of points of interest in" <<
+		std::endl << "a geographical space. Eagle Eye is" <<
+		std::endl << "specialized towards forest management." << 
+		std::endl <<
+		std::endl << "Developed by Ahmed Khan, Gavin Silva," <<
+		std::endl << "and Adam Freed from Ohlone College CS-124" <<
+		std::endl;
+	};
+	addCommand("info", infoLambda);
 }
 
 void Runtime::greetMessage()
 {
 	std::cout <<
 	std::endl << "Welcome to Eagle Eye." << 
+	std::endl << 
 	std::endl << "Type 'help' for list of commands" <<
 	std::endl << "Type 'exit' to terminate program" <<
 	std::endl;
@@ -44,8 +61,8 @@ void Runtime::greetMessage()
 
 void Runtime::invalidCommandMessage()
 {
-
-	std::cout << "Invalid command, try again." <<
+	std::cout <<
+	std::endl << "invalid command, try again." <<
 	std::endl;
 }
 
@@ -53,8 +70,6 @@ void Runtime::run()
 {
 	addLambdas();
 	greetMessage();
-	run_state = true;
-	
 	while(run_state)
 	{
 		std::cout << std::endl << "> ";
---
+++ src/Waypoint.hpp
@@ -0,0 +1,14 @@
+#ifndef #WAYPOINT_HPP
+#define #WAYPOINT_HPP
+
+class Waypoint
+{
+public:
+
+private:
+	int x;
+	int y;
+	std::string name;
+};
+
+#endif //WAYPOINT_HPP
