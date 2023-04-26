ef3931abdf54028c8545edd8f9dd54392e3e6375
hydraxl <adam.freed123@gmail.com>
2023-04-26 10:49:08 -0700

    moved code files to src

--- src/Waypoint.hpp
+++ src/Waypoint.hpp
@@ -1,14 +1,30 @@
-#ifndef #WAYPOINT_HPP
-#define #WAYPOINT_HPP
+#ifndef WAYPOINT_H
+#define WAYPOINT_H
 
-class Waypoint
-{
-public:
+#include <string>
+#include <unordered_map>
 
+class Waypoint {
 private:
-	int x;
-	int y;
-	std::string name;
+    std::string name;
+    int x;
+    int y;
+    unordered_map<string, int> features;
+
+public:
+    Waypoint();
+    Waypoint(std::string name, int x, int y);
+
+    std::string get_json() const;
+    std::string get_name() const;
+    int get_x() const;
+    int get_y() const;
+    int get_feature(std::string feature) const;
+
+    void add_feature(std::string feature, int val);
+    bool remove_feature(std::string feature);
+    void set_position(int x, int y);
+    void set_name(std::string name);
 };
 
-#endif //WAYPOINT_HPP
+#endif
