e46825444cbeb33afd16c932d390c7f0c4137e02
hydraxl <adam.freed123@gmail.com>
2023-05-04 10:26:13 -0700

    added hashmap

---
+++ src/hashmap.hpp
@@ -0,0 +1,46 @@
+#ifndef HASHMAP_HPP
+#define HASHMAP_HPP
+#include "waypoint.hpp"
+#include <string>
+
+
+class hashmap
+{
+public:
+
+	~hashmap();
+    hashmap(); //constructor to set consts
+
+    void insert(Waypoint data);
+    // adds a new element to the hash map
+
+    Waypoint find(std::string name) const;
+    // finds the Waypoint that has a given name
+
+    int hasher(const std::string name) const;
+	void resize_if_necessary();
+    int bucket_count() const;
+    int element_count() const;
+    void print() const;
+private:
+
+	// Node for sheparate chaining
+	struct hash_node
+	{
+		Waypoint data;
+		//pair of char, and cipher string key-value pairs
+		hash_node* next = nullptr;
+		//hash node is a linked list element
+	};
+
+
+	const double load_factor;
+	const int minimum_array_size;
+	int current_array_size;
+	int current_num_elements;
+	const char nullchar;
+	const int multiplier;
+	hash_node** table;
+};
+
+#endif //HASHMAP_HPP
