e9c6f4e042bcd88d729a68b1416d46145f1826cf
Ahmed <emailahmedebadkhan@gmail.com>
2023-05-07 09:15:36 -0700

    minor cleanup

--- src/Quicksort.hpp
+++ src/Quicksort.hpp
@@ -7,7 +7,6 @@
 class Quicksort
 {
 public:
-
     static int partition(std::vector<Waypoint*>& a, int from, int to, std::string type);
     static void quicksort(std::vector<Waypoint*>& a, std::string type);
     static void _quicksort(std::vector<Waypoint*>& a, int from, int to, std::string type);
